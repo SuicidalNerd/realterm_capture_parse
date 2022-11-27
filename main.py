1544a72fc6faea70cbe40ab82b2db1a27ace0942
SuicidalNerd <martincicmateo@gmail.com>
2022-11-27 23:43:42 +0100

    added average current

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from csv import reader, DictReader
 from datetime import datetime
 import decimal
 
-time_temp = ""
-current_temp = 0
+time = ""
+current = 0
 
+average_current = 0
+current_pom = 0
+measurement_counter = 0
 
 with open('capture_demo.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
@@ -23,9 +26,15 @@ with open('capture.csv','r') as read_obj:
      with open('results.csv', 'w') as results:
        for row in csv_dict_reader:
            #final_line = datetime.utcfromtimestamp(int(row['time'])).strftime('%H:%M:%S'), decimal.Decimal(row['current'])
-           time_temp = datetime.utcfromtimestamp(int(row['time'])).strftime('%H:%M:%S')
-           current_temp = decimal.Decimal(row['current'])
+           time = datetime.utcfromtimestamp(int(row['time'])).strftime('%H:%M:%S')
+           current = decimal.Decimal(row['current'])
            #print(final_line)
-           print(time_temp + "," + str(current_temp))
+           measurement_counter = measurement_counter + 1
+           current_pom = current_pom + current
+           print(time + "," + str(current))
            writer = csv.writer(results)
-           writer.writerow((time_temp, current_temp))
+           writer.writerow((time, current))
+           
+average_current = current_pom / measurement_counter
+average_current_ma = average_current/1000
+print("Average current:" + str(average_current) + " A - " +"(" + str(round(average_current_ma, 4)) +") mA")
